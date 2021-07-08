5929afd2f6dc9dfaa0bb0a123407e036808f016b
FranzAlec <dfranzalec@gmail.com>
2021-07-08 15:13:42 +0800

    Added animations for clicking icons and changing color of the circle depending on the flavor

---
+++ style.js
@@ -0,0 +1,10 @@
+function imgSlider (res) {
+    document.querySelector('.caramel').src = res;
+}
+
+function changeCircleColor (res) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = res;
+}
+
+
